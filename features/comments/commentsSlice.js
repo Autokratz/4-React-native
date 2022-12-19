940811ad0654b29d0b1328a680b3227f9e5fdea5
Hector Cabra <hectorcabra95@gmail.com>
2022-12-18 23:02:22 -0500

    Workshop Week 2

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -1,24 +1,33 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { baseUrl } from '../../shared/baseUrl';
-
 export const fetchComments = createAsyncThunk(
     'comments/fetchComments',
     async () => {
         const response = await fetch(baseUrl + 'comments');
-        if (!response.ok) {
-            return Promise.reject(
-                'Unable to fetch, status: ' + response.status
-            );
-        }
-        const data = await response.json();
-        return data;
+        return response.json();
+    }
+);
+
+export const postComment = createAsyncThunk(
+    'comments/postComment',
+    async (payload, {dispatch, getState}) => {
+        setTimeout(() => {
+            const {comments} = getState();
+            payload.date = new Date().toISOString();
+            payload.id = comments.commentsArray.length;
+            dispatch(addComment(payload));
+        }, 2000);
     }
 );
 
 const commentsSlice = createSlice({
     name: 'comments',
-    initialState: { isLoading: true, errMess: null, commentsArray: [] },
-    reducers: {},
+    initialState: {isLoading: true, errMess: null, commentsArray: []},
+    reducers: {
+        addComment: (state, action) => {
+            state.commentsArray.push(action.payload);
+        }
+    },
     extraReducers: {
         [fetchComments.pending]: (state) => {
             state.isLoading = true;
@@ -30,11 +39,11 @@ const commentsSlice = createSlice({
         },
         [fetchComments.rejected]: (state, action) => {
             state.isLoading = false;
-            state.errMess = action.error
-                ? action.error.message
-                : 'Fetch failed';
+            state.errMess = action.error ? action.error.message : 'Fetch failed';
         }
     }
 });
 
+
 export const commentsReducer = commentsSlice.reducer;
+export const {addComment} = commentsSlice.actions;
